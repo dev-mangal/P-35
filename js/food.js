f7d80ca59feb1a3c4277d797b6a65170c653c536
dev-mangal <69078514+dev-mangal@users.noreply.github.com>
2020-11-07 18:44:48 +0530

    Add files via upload

---
+++ js/food.js
@@ -0,0 +1,34 @@
+class Food{
+    constructor(){
+        this.foodstock=0;
+        this.lastfed;
+        this.image=loadImage("images/Milk.png");
+    }
+    deductfood(){
+        if(this.foodstock>0){
+         this.foodstock-=1;  
+        }
+    }
+    updatefood(stock){
+        this.foodstock=stock;
+    }
+    updatetime(time){
+        this.lastfed=time;
+    }
+    display(){
+        var x=50;
+        var y=100;
+        imageMode(CENTER);
+        
+        if(this.foodstock>0){
+            for(var i=0;i<this.foodstock;i++){
+                if(i%10===0){
+                    x=50;
+                    y+=50;
+                }
+                image(this.image,x,y+30,30,30);
+                x+=30;
+            }
+        }
+    }
+}
